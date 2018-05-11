5af4818dec4a6157844af234bcef71e00db3ce4c
Grayson Hicks <graysonhicks@gmail.com>
2018-05-10 22:24:09 -0400

    own responsive nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,53 +1,118 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
-import ResponsiveMenu from 'react-responsive-navbar'
+import ScrollLock from 'react-scrolllock'
 import { TiThMenu, TiTimes } from 'react-icons/lib/ti'
 import mobileLogo from '../assets/images/mobile_logo.svg'
+import styled from 'styled-components'
+
+const SmallMenu = styled.div`
+  display: none;
+  @media (max-width: ${props => props.breakpoint}) {
+    display: flex;
+  }
+`
+
+const LargeMenu = styled.div`
+  @media (max-width: ${props => props.breakpoint}) {
+    display: none;
+  }
+`
+
+const Menu = ({ handleClick }) => {
+  return (
+    <ul className="nav-links-container">
+      <li className="nav-links logo-container">
+        <Link to="/" onClick={handleClick}>
+          <img src={mobileLogo} className="logo" alt="" />
+        </Link>
+      </li>
+      <li className="nav-links">
+        <Link to="/#mission" onClick={handleClick}>
+          mission
+        </Link>
+      </li>
+      <li className="nav-links">
+        <Link to="/#who-and-why" onClick={handleClick}>
+          who and why
+        </Link>
+      </li>
+      <li className="nav-links">
+        <Link to="/#how" onClick={handleClick}>
+          programs
+        </Link>
+      </li>
+      <li className="nav-links">
+        <Link to="/faq" onClick={handleClick}>
+          faq
+        </Link>
+      </li>
+    </ul>
+  )
+}
 
 class Nav extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      isOpen: false,
+      showMenu: false,
+      scrollLock: false,
+      breakpoint: '736px',
     }
   }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+  }
+  componentWillUnmount() {
+    window.addEventListener('resize', null)
+  }
+
+  handleResize = () => {
+    if (window.innerWidth >= 736) {
+      this.setState({
+        showMenu: false,
+        scrollLock: false,
+      })
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      showMenu: !this.state.showMenu,
+      scrollLock: !this.state.scrollLock,
+    })
+  }
+
   render() {
     return (
       <div>
         <Link to="/">
           <img src={mobileLogo} className="mobile-logo" alt="" />
         </Link>
-
-        <ResponsiveMenu
-          menuOpenButton={<TiThMenu />}
-          menuCloseButton={<TiTimes />}
-          changeMenuOn="736px"
-          largeMenuClassName="desktop-nav"
-          smallMenuClassName="mobile-nav"
-          menu={
-            <ul className="nav-links-container">
-              <li className="nav-links logo-container">
-                <Link to="/">
-                  <img src={mobileLogo} className="logo" alt="" />
-                </Link>
-              </li>
-              <li className="nav-links">
-                <Link to="/#mission">mission</Link>
-              </li>
-              <li className="nav-links">
-                <Link to="/#who-and-why">who and why</Link>
-              </li>
-              <li className="nav-links">
-                <Link to="/#how">programs</Link>
-              </li>
-              <li className="nav-links">
-                <Link to="/faq">faq</Link>
-              </li>
-            </ul>
-          }
-        />
+        <LargeMenu className="desktop-nav" breakpoint={this.state.breakpoint}>
+          <Menu handleClick={this.handleClick} />
+        </LargeMenu>
+        <SmallMenu
+          className={`mobile-nav ${this.state.showMenu ? 'open' : ''}`}
+          breakpoint={this.state.breakpoint}
+        >
+          {!this.state.showMenu ? (
+            <div role="button">
+              <TiThMenu onClick={this.handleClick} />
+            </div>
+          ) : (
+            <div role="button">
+              <TiTimes onClick={this.handleClick} />
+            </div>
+          )}
+          {this.state.showMenu ? (
+            <div>
+              <Menu handleClick={this.handleClick} />
+            </div>
+          ) : null}
+        </SmallMenu>
+        {this.state.scrollLock ? <ScrollLock /> : null}
       </div>
     )
   }
